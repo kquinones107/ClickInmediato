68c98e7e9649198702876b909f19db61bb14cdca
kquinones107 <kquinones@unal.edu.co>
2025-03-29 12:31:30 -0500

    feature:authcontex

---
+++ frontend/src/context/AuthContex.jsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+
+
+    useEffect(() => {
+        const savedToken = localStorage.getItem("token");
+        const savedUser = localStorage.getItem("user");
+
+        if (savedToken && savedUser) {
+            setToken(savedToken);
+            setUser(JSON.parse(savedUser));
+        }
+    }, []);
+
+    const login = (userData, token) => {
+        setUser(userData);
+        setToken(token);
+        localStorage.setItem("user", JSON.stringify(userData));
+        localStorage.setItem("token", token);
+    };
+
+    const logout = () => {  
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, token, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
